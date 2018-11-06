e6ff809bb69bbf32cb7f09dcf03dd1a03aaa2fcc
Zaira Gonzalez <zgonzalez@nyc.yearup.org>
2018-11-06 08:47:02 -0500

    HTML and CSS thingz

--- w03/d1/morning_exercise/main.js
+++ w03/d1/morning_exercise/main.js
@@ -1,1 +1,15 @@
-// Write a program that will `console.log` the numbers from 1 to 100. But for multiples of three `console.log` “Fizz” instead of the number and for the multiples of five `console.log` “Buzz”. For numbers which are multiples of both three and five `console.log` “FizzBuzz”.
+// // Write a program that will `console.log` the numbers from 1 to 100.
+// But for multiples of three `console.log` “Fizz” instead of the number
+//  and for the multiples of five `console.log` “Buzz”.
+//   For numbers which are multiples of both three and five `console.log` “FizzBuzz”.
+
+
+for (var i = 0; i<=100; i++){
+  if (i%3 === 0){
+    console.log("Fizz");
+  if (i%5 === 0)
+    console.log("Buzz");
+}
+  console.log(i);}
+
+// }
