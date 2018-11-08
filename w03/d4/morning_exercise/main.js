1551becf1df5865c4958b028b3af95d1e4b72c71
Fizal Sarif <fizal.sarif@gmail.com>
2018-11-07 22:12:35 -0500

    morning exercise

---
+++ w03/d4/morning_exercise/main.js
@@ -0,0 +1,18 @@
+//RANDOM ITEM FROM AN ARRAY https://css-tricks.com/snippets/javascript/select-random-item-array/
+
+const quotes = [
+  '"I’m not a businessman, I’m a business… man."– Jay Z ',
+  '"Its Cool To Love To Win, But Its Better To Hate To Lose"– Nas',
+  '"YOU CAN SPEND HOURS , DAYS , WEEKS , OR EVEN MONTHS TRYING TO PICK UP THE PIECES AND FIGURE OUT WHAT WENT WRONG OR YOU CAN JUST LEAVE THE PIECES ON THE FLOOR AND MOVE THE F@CK ON " - 2Pac',
+  '"The most important parts,are the ones that are unseen. The wings don’t make you fly, and the crown don’t make you king." - Lupe',
+  '"I sell ice in the winter, I sell fire in hell, I am a hustler baby, I’ll sell water to a well" - Jay Z'
+];
+
+function randomQuote(){
+
+}
+
+let button = document.querySelector('button');
+button.addEventListener('click', randomQuote);
+
+// randomQuote();
